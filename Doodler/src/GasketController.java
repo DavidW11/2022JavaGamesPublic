1d5dc7fb3f28304b307acb1717a1c0378054dca6
DavidW11 <70180345+DavidW11@users.noreply.github.com>
2022-01-26 14:20:25 -0500

    Final

--- Doodler/src/GasketController.java
+++ Doodler/src/GasketController.java
@@ -2,11 +2,14 @@
  * 
  * 1)
  * Program A takes less time than B for all values of 0.001 < n < 29.718.
- * When graphing A = 2^n/1000 and B = 1000*n^2, A is less than B for all values of n
- * greater than 0.001 and less than 29.718
+ * When graphing the functions A = 2^n/1000 and B = 1000*n^2, 
+ * A is less than B for all values of n greater than 0.001 and less than 29.718
  * 
  * 2)
- * 
+ * The complexity is O(n)
+ * Because the program iterates through each character of the 2 strings in order to create
+ * a new string of length n + n. In other words, the program iterates through 2n characters.
+ * Thus, the complexity can be written as O(2n), or O(n).
  * 
  * 3)
  	public static int Binary (int num){
