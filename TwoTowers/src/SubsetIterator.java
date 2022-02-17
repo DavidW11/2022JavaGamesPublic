61d70c0d58a3362b9a158d3a441e36b620c7dbaa
DavidW11 <70180345+DavidW11@users.noreply.github.com>
2022-02-17 14:29:54 -0500

    Base

--- TwoTowers/src/SubsetIterator.java
+++ TwoTowers/src/SubsetIterator.java
@@ -1,13 +1,3 @@
-/* Thought Questions:
- * 
- * 1)
- * 
- * 
- * 2)
- * 
- * 
- */
-
 import structure5.*;
 
 public class SubsetIterator extends AbstractIterator<Vector<Double>>{
@@ -29,8 +19,8 @@ public class SubsetIterator extends AbstractIterator<Vector<Double>>{
 		return counter >= 0 && counter <= (Math.pow(2, v.size()) - 1);
 	}
 	
+	// gets subset
 	public Vector<Double> get() {
-		
 		Vector<Double> newSubset = new Vector<Double>();
 		
 		for(int p = 0; p<v.size(); p++) {
@@ -39,38 +29,12 @@ public class SubsetIterator extends AbstractIterator<Vector<Double>>{
 		}
 		
 		return newSubset;
-		
-		/*
-		// create vector of bits from the long subset
-		Vector<Integer> bitSubset = new Vector<Integer>();
-		int bitLength = (int) (Math.log(counter)/Math.log(2));
-		for (int bitNum = 0; bitNum < bitLength; bitNum++) {
-			int bit = (int) counter & (1<<bitNum);
-			bitSubset.add(bit);
-		}
-		
-		// create subset of values using bit vector
-		// if bit is 1, add the corresponding value to the subset vector
-		Vector<Double> result = new Vector<Double>();
-		for (int i = 0; i < bitSubset.size(); i++) {
-			if (bitSubset.get(i) == 1) result.add(v.get(i));
-		}
-		
-		return result;
-		*/
 	}
 	
+	// returns next subset
 	public Vector<Double> next() {
 		subset = get();
 		counter++;
 		return subset;
-	}
-	
-	public void printSubset() {
-		//System.out.println(counter);
-		for(Double d : subset) {
-			System.out.print(d + " ");
-		}
-		System.out.println();
 	}
 }
--- TwoTowers/src/TwoTowers.java
+++ TwoTowers/src/TwoTowers.java
@@ -1,17 +1,73 @@
+/* Thought Questions:
+ * 
+ * 1)
+ * 
+ * 
+ * 2)
+ * 
+ * 
+ */
+
 import structure5.Vector;
 
 public class TwoTowers {
 	
+	private final static int N = 15;
+	private static Vector<Double> tower;
+	
 	public static void main(String[] args) {
-		Vector<Double> tower = new Vector<Double>();
-		tower.add(1.0);
-		tower.add(2.0);
-		tower.add(3.0);
+		// vector of block heights from sqrt(1) to sqrt(N)
+		tower = new Vector<Double>();
+		for(int i = 1; i<N; i++) {
+			tower.add(Math.sqrt(i));
+		}
 		
 		SubsetIterator si = new SubsetIterator(tower);
+		
+		// create vector of subsets
+		Vector<Vector<Double>> subsets= new Vector<Vector<Double>>();
 		while (si.hasNext()) {
-			si.next();
-			si.printSubset();
+			subsets.add(si.next());
 		}
+		
+		// find the smallest difference between the height of a subset and total height/2
+		double smallestDiff = diff(subsets.get(0));
+		// remember the subset that gets the closest
+		Vector<Double> towerA = subsets.get(0);
+		for(int i = 0; i<subsets.size(); i++) {
+				if( diff(subsets.get(i)) < smallestDiff ) {
+					smallestDiff = diff(subsets.get(i));
+					towerA = subsets.get(i);
+				}
+		}
+		
+		// print results
+		System.out.print("Best Tower: ");
+		printTower(towerA);
+		
+		System.out.print("Height Difference: ");
+		System.out.println(smallestDiff);
+	}
+	
+	// finds sum of a vector
+	public static double sum(Vector<Double> v) {
+		double sum = 0;
+		for(Double d:v) {
+			sum+=d;
+		}
+		return sum;
+	}
+	
+	// finds difference between height of subset and total height/2
+	public static double diff(Vector<Double> v) {
+		return Math.abs(sum(v) - sum(tower)/2);
+	}
+	
+	// prints the areas of each block in the tower
+	public static void printTower(Vector<Double> v) {
+		for(Double d:v) {
+			System.out.print(Math.round(Math.pow(d, 2)) + " ");
+		}
+		System.out.println();
 	}
 }
